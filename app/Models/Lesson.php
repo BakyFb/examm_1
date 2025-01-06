9499e32b6ffab350abf5b5d87b5105f2a3966f68
bakyfb <bakyurayew2@gmail.com>
2025-01-06 20:16:45 +0500

    third commit

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -9,4 +9,25 @@ class Lesson extends Model
 {
     /** @use HasFactory<\Database\Factories\LessonFactory> */
     use HasFactory;
+    protected $guarded = ['id'];
+    public function students()
+    {
+        return $this->belongsTo(Student::class, 'registrations');
+    }
+    public function teachers()
+    {
+        return $this->belongsTo(Teacher::class, 'registrations');
+    }
+    public function courses()
+    {
+        return $this->belongsTo(Course::class, 'registrations');
+    }
+    public function seasons()
+    {
+        return $this->belongsTo(Season::class, 'registrations');
+    }
+    public function locations()
+    {
+        return $this->belongsTo(Location::class, 'registrations');
+    }
 }
